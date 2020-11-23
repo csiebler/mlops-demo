01605e9ad7d55942ec92650a104af08427ea867a
Clemens Siebler <clemens.siebler@gmail.com>
2020-11-23 09:40:43 +0100

    Added inference_schema for endpoint creation

--- models/german-credit-basic/score.py
+++ models/german-credit-basic/score.py
@@ -2,6 +2,8 @@ import json
 import numpy as np
 import pandas as pd
 import joblib
+from inference_schema.schema_decorators import input_schema, output_schema
+from inference_schema.parameter_types.standard_py_parameter_type import StandardPythonParameterType
 from azureml.core.model import Model
 
 MODEL_NAME = 'german-credit-basic-model'
@@ -11,11 +13,27 @@ def init():
     model_path = Model.get_model_path(MODEL_NAME)
     model = joblib.load(model_path)
 
+# Automatically generate the swagger interface by providing an data example
+input_sample = [{
+    "Age": 20,
+    "Sex": "male",
+    "Job": 0,
+    "Housing": "own",
+    "Saving accounts": "little",
+    "Checking account": "little",
+    "Credit amount": 100,
+    "Duration": 48,
+    "Purpose": "radio/TV"
+  }]
+output_sample = [[0.7, 0.3]]
+
+@input_schema('data', StandardPythonParameterType(input_sample))
+@output_schema(StandardPythonParameterType(output_sample))
 def run(raw_data):
     try:
-        data = json.loads(raw_data)['data']
-        input_df = pd.DataFrame.from_dict(data)
-        proba = model.predict_proba(input_df)
+        df = pd.DataFrame(raw_data)
+        proba = model.predict_proba(df)
+        
         result = {"predict_proba": proba.tolist()}
         return result
     except Exception as e:
